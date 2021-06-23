f233c9629f982e72ee6e378e827c8a28c6d1a637
crpoudyal <crpoudyal@gmail.com>
2021-06-23 12:33:09 +0545

    navbar login btn fixed

--- require/header.php
+++ require/header.php
@@ -1,12 +1,12 @@
 <header>
     <a href="index.php" class="logo"><i class="fas fa-clinic-medical"></i>Medic Nepal</a>
-    <div id="menu-bar" class="fas fa-bars"></div>
     <nav class="navbar">
         <a href="index.php">Home</a>
         <a href="medicine.php">Medicine</a>
         <a href="pharmacy.php">Pharmacy</a>
     </nav>
     <div id="nav-icon">
-        <a href="login.php" class="login-btn">Login</a>
-    </div>
+    <a href="login.php" id="login-b" class="login-btn">Login</a>  
+    </div> 
+    <div id="menu-bar" class="fas fa-bars"></div>
 </header>
