38862423ef500b0baac5358fe9545be11b10aa5d
crpoudyal <crpoudyal@gmail.com>
2021-06-30 22:31:25 +0545

    changes

--- Template/product.php
+++ Template/product.php
@@ -4,14 +4,16 @@ $get_product_data =$Fetch_medicine ->getData($table = "Medicine");
 
 
 <section class="product" id="product-top">
-    <h1 class="heading">COVID-19 Essentials</h1>
+    <h1 class="heading"><?php  
+        echo $get_category_data['c_name'] ?>
+    </h1>
     <div class="box-container">
-    <?php foreach($get_product_data as $m){ ?>
+    <?php foreach($get_product_data as $data){ ?>
         <div class="box" onclick="location.href='details.php'">
             <span class="offer">10% off</span>
-            <img src="<?php echo $m['img_name'] ?? "images/ensure.png" ?>" alt="">
-            <h1><?php echo $m['m_name'] ?? "unknown"?></h1>
-            <p id="price">Rs.<span id="del">70</span> <?php echo $m['price']?></p>
+            <img src="<?php echo $data['img_name'] ?? "images/ensure.png"?>" alt="">
+            <h1><?php echo $data['m_name'] ?? "unknown"?></h1>
+            <p id="price">Rs.<span id="del">70</span> <?php echo $data['price']?></p>
             <a href="#" class="btn">Add to Cart</a>     
         </div>
         <?php }?>
--- database/Fetch_medicine.php
+++ database/Fetch_medicine.php
@@ -11,21 +11,21 @@ class Fetch_medicine
         $this->db = $db;
     }
 
-    // fetch medicine data using getData Method
-    public function getData($table = 'Medicine'){
+    // fetch data using getData Method
+    public function getData($table){
         $result = $this->db->con->query("SELECT * FROM {$table}");
 
         $resultArray = array();
 
-        // fetch product data one by one
-        while ($m = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            $resultArray[] = $m;
+        // fetch data one by one
+        while ($data = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $resultArray[] = $data;
         }
 
         return $resultArray;
     }
 
-    // get product using medicine id
+    // get medicine using medicine id
     public function getMedicine($m_id = null, $table= 'Medicine'){
         if (isset($m_id)){
             $result = $this->db->con->query("SELECT * FROM {$table} WHERE m_id={$m_id}");
