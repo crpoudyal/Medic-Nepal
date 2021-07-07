ef4d0cacf113b888fdd275c4d339bcc593c32a0e
crpoudyal <crpoudyal@gmail.com>
2021-07-07 09:02:45 +0545

    pop_category dynamic data

--- Template/pop_category.php
+++ Template/pop_category.php
@@ -1,13 +1,22 @@
+<?php 
+require 'database/DBController.php';
+require 'function.php';
+$get_category_data = get_Data($con,'Category');
+?>
+
+
 <section class="category" id="category">
     <h1 class="heading"> Popular <span>Category</span> </h1><br/>
     <div class="box-container">
+    <?php foreach($get_category_data as $c){ ?>
         <div class="box">
             <div class="content">
                 <img src="images/covid.png" alt="">
-                <h3>Diabeties</h3>
-                <p>details</p>
+                <h3><?php echo $c['c_name'] ?></h3>
+                <p><?php echo $c['c_details'] ?></p>
                 <a href="#" class="btn">Know More</a>
             </div>
         </div>
+        <?php }?>
     </div>
 </section>
